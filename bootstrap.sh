9a87db2cd10203563a12dd6b9a5a12320dba6990
Alex Uprise <alex.uprise@gmail.com>
2018-11-14 22:34:12 +0300

    Bootstrap tweak

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,20 +4,23 @@ apt-add-repository ppa:ondrej/php
 
 apt-get update
 
+apt-get install -y git
+
 apt-get install -y apache2
 apt-get install -y libapache2-mod-fcgi
+
 apt-get install -y php7.0
 apt-get install -y php7.0-fpm
 
 a2enmod php7.0
 
-service apache2 restart
-
-apt-get install -y git
+service apache2 stop
 
 rm -rf /var/www/html
 
 git clone https://github.com/alexuprise/log-grid.git /var/www/html
+
+service apache2 start
 
 cd /var/www/html
 
